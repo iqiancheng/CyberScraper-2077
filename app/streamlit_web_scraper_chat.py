270d091d856b82d3dc6a11dc2d096b65ad222ce0
qian.cheng <mgr.qiancheng@gmail.com>
2024-08-20 21:26:31 +0800

    Dockerfile & self hosted via docker-compose

--- app/streamlit_web_scraper_chat.py
+++ app/streamlit_web_scraper_chat.py
@@ -1,9 +1,12 @@
+import os
 import asyncio
 import streamlit as st
 from src.web_extractor import WebExtractor
 
 class StreamlitWebScraperChat:
-    def __init__(self, model_name):
+    def __init__(self, model_name: str = "gpt-3.5-turbo"):
+        specific_model_name = os.getenv("MODEL_NAME")
+        model_name = specific_model_name if specific_model_name else model_name
         self.web_extractor = WebExtractor(model_name=model_name)
 
     def process_message(self, message: str) -> str:
